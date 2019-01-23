7646a6f78d3a2730aee1a128b6f01894275fb90b
Ben Toews <mastahyeti@gmail.com>
2019-01-23 11:43:06 -0700

    dsa signature verification

--- spec/public_key/dsa_spec.rb
+++ spec/public_key/dsa_spec.rb
@@ -1,12 +1,19 @@
 require_relative "../spec_helper"
 
 describe SSHData::PublicKey::DSA do
-  let(:private_key) { OpenSSL::PKey::DSA.generate(2048) }
+  let(:private_key) { OpenSSL::PKey::DSA.generate(1024) }
   let(:public_key)  { private_key.public_key }
   let(:params)      { public_key.params }
 
-  let(:cert)   { SSHData::Certificate.parse(fixture("rsa_leaf_for_dsa_ca-cert.pub"), unsafe_no_verify: true) }
-  let(:ca_key) { cert.ca_key }
+  let(:msg)         { "hello, world!" }
+  let(:digest)      { OpenSSL::Digest::SHA1.new }
+  let(:openssl_sig) { private_key.sign(digest, msg) }
+  let(:ssh_sig)     { described_class.ssh_signature(openssl_sig) }
+  let(:sig)         {
+    SSHData::Encoding.encode_signature(SSHData::PublicKey::ALGO_DSA, ssh_sig)
+  }
+
+  let(:openssh_key) { SSHData::PublicKey.parse(fixture("dsa_leaf_for_rsa_ca.pub")) }
 
   subject { described_class.new(p: params["p"], q: params["q"], g: params["g"], y: params["pub_key"]) }
 
@@ -22,8 +29,25 @@ describe SSHData::PublicKey::DSA do
     expect(subject.openssl.to_der).to eq(public_key.to_der)
   end
 
+  it "can encode/decode signatures" do
+    round_tripped = described_class.openssl_signature(
+      described_class.ssh_signature(openssl_sig)
+    )
+
+    expect(round_tripped).to eq(openssl_sig)
+  end
+
+  it "can verify signatures" do
+    expect(subject.verify(msg, sig)).to be(true)
+  end
+
   it "can parse openssh-generate keys" do
-    expect(ca_key).to be_a(described_class)
-    expect { ca_key.openssl }.not_to raise_error
+    expect { openssh_key.openssl }.not_to raise_error
+  end
+
+  it "can verify certificate signatures" do
+    SSHData::Certificate.parse(fixture("rsa_leaf_for_dsa_ca-cert.pub"),
+      unsafe_no_verify: false
+    )
   end
 end
