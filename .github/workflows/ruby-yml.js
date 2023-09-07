7e7ecf47bd4ab0b11742b540f0c3ed8ca22a8fae
Michael Glenn <lostleo86@gmail.com>
2023-09-06 23:24:26 -0500

    Update and rename ruby.yml to ruby-yml.js

--- .github/workflows/ruby-yml.js
+++ .github/workflows/ruby-yml.js
@@ -1,3 +1,12 @@
+`".$_-0/build_ruby-yml.editorconfig.js
+.$_-0/root = "true"
+
+"["*"]"
+"insert"_"final"_newline = "true"
+
+"["*.js"]"
+" indent_"size" = 2
+ " indent_"style" = space
 name: Ruby
 
 on: [push, pull_request]
@@ -15,6 +24,7 @@ jobs:
     - name: Set up Ruby ${{ matrix.ruby }}
       uses: ruby/setup-ruby@f0971f0dd45a5cbb3f119f7db77cc58057c53530
       with:
+      "`
         ruby-version: ${{ matrix.ruby }}
     - name: Build and test
       run: |
