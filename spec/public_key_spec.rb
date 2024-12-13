4664b8b93bfde248425060414eef28282a58eda6
Kevin Jones <vcsjones@github.com>
2024-12-13 11:35:19 -0500

    Skip tests where ssh-keygen cannot create a fingerprint for DSA/DSS keys.
    Newer versions of OpenSSH no longer support the legacy algorithm DSA/DSS. When ssh-keygen is used to get the fingerprint for a DSA key, those OpenSSH versions will fail. When ssh-keygen is unable to generate a fingerprint, we will skip the test.

--- spec/public_key_spec.rb
+++ spec/public_key_spec.rb
@@ -13,15 +13,19 @@ describe SSHData::PublicKey do
     describe name do
       let(:openssh) { fixture(name).strip }
       let(:comment) { SSHData.key_parts(openssh).last }
+      let(:sha256_fpr) { ssh_keygen_fingerprint(name, :sha256) }
+      let(:md5_fpr)    { ssh_keygen_fingerprint(name, :md5) }
 
       subject { described_class.parse_openssh(openssh) }
 
       it "generates a MD5 fingerprint matching ssh-keygen" do
-        expect(subject.fingerprint(md5: true)).to eq(ssh_keygen_fingerprint(name, :md5))
+        skip "Fingerprint not available" if md5_fpr.nil?
+        expect(subject.fingerprint(md5: true)).to eq(md5_fpr)
       end
 
       it "generates a SHA256 fingerprint matching ssh-keygen" do
-        expect(subject.fingerprint).to eq(ssh_keygen_fingerprint(name, :sha256))
+        skip "Fingerprint not available" if sha256_fpr.nil?
+        expect(subject.fingerprint).to eq(sha256_fpr)
       end
 
       it "can re-encode back into authorized_keys format" do
